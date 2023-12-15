a67572877dcf0f2ec025ced1ecdf7775edc83c46
GauravKumar260100 <gaurav.kumar.mcs21@cs.du.ac.in>
2023-12-15 22:26:35 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo({ width = "100px" }) {
+  return (
+    <div>
+      <h1 className="my-1 text-gray-800 font-bold text-2xl">HydrogenBlogs</h1>
+    </div>
+  );
+}
+
+export default Logo;
